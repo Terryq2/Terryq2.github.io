26f7c62677bfd92c43f97b801d3470df726554cb
Terryq2 <terryq2@illinois.edu>
2024-08-26 08:52:46 -0500

    Format and code quality
    Comments on read more function and formatting.

--- Script&Style/readMorePublications.js
+++ Script&Style/readMorePublications.js
@@ -1,19 +1,27 @@
 function readMore(name) {
-    var contentStatus = document.querySelector(`.item[name="${name}"] .hiddenContent`);
-    var buttonText = document.querySelector(`.item[name="${name}"] .button`);
-    var spacing = document.querySelector(`.item[name="${name}"] .spacing`);
-    var arrow = document.querySelector(`.item[name="${name}"] .arrow`);
-    var buttonContainer = document.querySelector(`.item[name="${name}"] .buttonContainer`);
-  
-    if (contentStatus.style.display === "none" || contentStatus.style.display === "") {
-      buttonText.innerHTML = "Collapse"; 
-      contentStatus.style.display = "block";
-      spacing.style.display = "inline";
-    } else {
-      buttonText.innerHTML = "More"; 
-      arrow.innerHTML = "&#9656";
-      contentStatus.style.display = "none";
-      spacing.style.display = "none";
-    }
+  var extraContentStatus = document.querySelector(
+    `.item[name="${name}"] .hiddenContent`,
+  );
+  var buttonText = document.querySelector(`.item[name="${name}"] .button`);
+  var spacing = document.querySelector(`.item[name="${name}"] .spacing`);
+  var arrow = document.querySelector(`.item[name="${name}"] .arrow`);
+
+  /*
+    If extra content is hidden initially, it will be displayed after the function is called.
+    If it is not hidden, it will be hidden after the function is called.
+  */
+
+  if (
+    extraContentStatus.style.display === "none" ||
+    extraContentStatus.style.display === ""
+  ) {
+    buttonText.innerHTML = "Collapse";
+    extraContentStatus.style.display = "block";
+    spacing.style.display = "inline";
+  } else {
+    buttonText.innerHTML = "More";
+    arrow.innerHTML = "&#9656";
+    extraContentStatus.style.display = "none";
+    spacing.style.display = "none";
   }
-  
+}
